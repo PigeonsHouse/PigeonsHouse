b5205bcce66b860c78f7dc14974f6d7464dc26b0
PigeonsHouse <bullbull1230@gmail.com>
2026-01-31 17:17:07 +0900

    fix: GlobalStyleの修正

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -1,8 +1,10 @@
 import { css, Global } from "@emotion/react";
 
 const style = css`
-  margin: 0;
-  background-color: #f0f0f0;
-  font-family: "Noto Sans JP", sans-serif;
+  body {
+    margin: 0;
+    background-color: #f0f0f0;
+    font-family: "Noto Sans JP", sans-serif;
+  }
 `;
 export const GlobalStyles = () => <Global styles={style} />;
